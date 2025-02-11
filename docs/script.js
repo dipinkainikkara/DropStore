c47da69d51a5996c06dc4352ca4dc23749b0bfba
Dipinmohan <dipinmohan@Dipinmohans-Mac-mini.local>
2025-02-11 17:06:14 +0000

    Moved files to docs folder for GitHub Pages

---
+++ docs/script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript to log added items to the console
+function addToCart(product) {
+    console.log(product + " has been added to the cart.");
+    alert(product + " has been added to the cart!");
+}
